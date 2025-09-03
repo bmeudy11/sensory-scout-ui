8b2c7d48e0137785b51ebf2ce31456608e04b906
bmeudy11 <bmeudy@gmail.com>
2025-09-03 13:09:16 -0400

    handle jwt on app.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,32 @@
-import React, 'useState';
+import React, { useState } from 'react';
 import * as SecureStore from 'expo-secure-store';
+
+import loginScreen from './loginScreen';
+import mainContent from './mainContent';
+
+export default function App() {
+    const [userToken, setUserToken] = useState(null);
+
+    const handleLoginSuccess = (token) => {
+        setUserToken(token);
+    };
+
+    const handleLogout = async() => {
+        //invalidate user token
+        await SecureStore.deleteItemAsync('userToken');
+        setUserToken(null);
+    };
+
+    return (
+        <>
+            {userToken == null ? (
+                    //show login if token doesn't exist
+                    <loginScreen onLoginSuccess={handleLoginSuccess} />
+                ) : (
+                    //if token exists show main content
+                    <mainContent token={userToken} onLogout={handleLogout} />
+                )
+            }
+        </>
+    );
+};
